d05f241b7971a076460f8c9a190e136f8547908e
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-10-18 22:07:29 +0100

    first func!

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-def
+#Find similarities between two sequences of same length
+def score_match(query, subject, match, mismatch):
+    score = 0;
+    for i in range(0, len(subject)):
+        if subject[i] == query[i]:
+            score += match;
+        else:
+            score += mismatch;
+
+    return score;
+
+subject = 'actgatcGATTGATCgatc'
+query   =    'tttaGATCGATCtttgatc'
+
+print(score_match(query, subject, 1, -1));
